6f1ad172ef3a6f627a7fe2a4cb20623f87cbaab2
nadahkhaledd <nadahkhaledd@gmail.com>
2020-12-25 23:29:30 +0200

    changes in classes

--- ProductRest/src/main/java/net/codejava/TemplateController.java
+++ ProductRest/src/main/java/net/codejava/TemplateController.java
@@ -1,9 +1,16 @@
 package net.codejava;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +23,37 @@ public class TemplateController {
 	public List<Template> list() {
 		return service.listAll();
 	}
-
+	
+	@GetMapping("/templates/{id}")
+	public ResponseEntity<Template> get(@PathVariable int id) {
+		try {
+			Template template = service.get(id);
+			return new ResponseEntity<Template>(template, HttpStatus.OK);
+		}
+		catch(NoSuchElementException e) {
+			return new ResponseEntity<Template>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	public void add(@RequestBody Template template) {
+		service.save(template);
+	}
+	
+	@PutMapping("/templates/{id}")
+	public ResponseEntity<?> get(@RequestBody Template template, @PathVariable int id) {
+		try {
+			Template existtemplate = service.get(id);
+			service.save(template);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		catch(NoSuchElementException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("/templates/{id}")
+	public void delete(@PathVariable int id) {
+		service.delete(id);
+	}
+	
 }
